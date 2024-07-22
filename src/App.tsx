4ae31d908005c8dcf1004140290585fd8cec5775
smirnov <smirnov@alfasatcom.ru>
2024-07-22 14:18:15 +0300

    инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+const App: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>App component</h1>
+      </Container>
+    </>
+  );
+};
+
+export default App;
